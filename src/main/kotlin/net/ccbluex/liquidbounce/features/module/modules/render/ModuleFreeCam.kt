54d438bf60e73fbba5c424e1ecd324fb479d8d7a
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-02-18 14:38:50 +0800

    26.1-snapshot-8 (2)

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleFreeCam.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleFreeCam.kt
@@ -151,7 +151,7 @@ object ModuleFreeCam : ClientModule("FreeCam", ModuleCategories.RENDER, disableO
 
         fun update(velocity: Vec3) = set(pos + velocity)
 
-        fun interpolate(tickDelta: Float) = lastPos.lerp(pos, tickDelta.toDouble())
+        fun interpolate(partialTicks: Float) = lastPos.lerp(pos, partialTicks.toDouble())
     }
 
     override fun onEnabled() {
@@ -254,14 +254,14 @@ object ModuleFreeCam : ClientModule("FreeCam", ModuleCategories.RENDER, disableO
         }
     }
 
-    fun applyCameraPosition(entity: Entity, tickDelta: Float) {
+    fun applyCameraPosition(entity: Entity?, partialTicks: Float) {
         if (!running || entity != player || !PositionState.available) {
             return
         }
 
         val camera = mc.gameRenderer.mainCamera
 
-        return camera.setPosition(PositionState.interpolate(tickDelta))
+        return camera.setPosition(PositionState.interpolate(partialTicks))
     }
 
     fun renderPlayerFromAllPerspectives(entity: LivingEntity): Boolean {
